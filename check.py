f60b4994f2a7713fe2281b011cfc05a95685dad9
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-13 14:48:38 +0200

    Update check.py

--- check.py
+++ check.py
@@ -42,3 +42,24 @@ def check_url(url):
     except Exception,e:
         LOGGER.warn('Check url error: '+str(e))
         exit(0)
+        
+        
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////7
+
+DESCRIPCION:
+    
+    Este es un script de Python que tiene dos funciones: check_install() y check_url(url).
+
+La función check_install() comprueba si el navegador Chrome o PhantomJS están instalados correctamente en el sistema. 
+En primer lugar, intenta iniciar una instancia de Chrome. Si esto falla, intenta iniciar una instancia de PhantomJS. 
+Si ambos intentos fallan, el script registra un mensaje de advertencia indicando que ningún navegador está instalado correctamente. 
+Si uno o ambos navegadores se inician correctamente, el script registra un mensaje indicando que los navegadores están instalados correctamente.
+
+La función check_url(url) comprueba si una URL proporcionada es accesible en línea. 
+Utiliza la biblioteca urllib2 de Python para abrir la URL proporcionada y, si se produce un error, el script registra un mensaje de advertencia indicando que la URL no se puede acceder.
+
+Además, el script importa dos módulos: urllib2 y selenium. 
+    El módulo urllib2 se utiliza en la función check_url() para acceder a una URL proporcionada, mientras que el módulo selenium se utiliza en la función check_install() para iniciar una instancia de Chrome o PhantomJS.
+    También importa un módulo personalizado llamado log, que contiene un objeto LOGGER utilizado para registrar mensajes de registro.
+        
+        
