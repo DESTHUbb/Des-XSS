f7afdd4edc77a595c24a5a086730b3c090e29eb2
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-13 15:55:41 +0200

    Update model.py

--- model.py
+++ model.py
@@ -103,3 +103,103 @@ class Case():
 
 if __name__ == '__main__':
     pass
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+DESCRIPCION:
+    
+Este es un archivo de Python que define tres clases: HttpRequest, HttpResponse y Case. Estas clases son modelos que se utilizan para representar objetos HTTP y casos de prueba.
+
+La clase HttpRequest representa una solicitud HTTP, y tiene atributos como method (método HTTP), url (URL de la solicitud), headers (encabezados de la solicitud) y body (cuerpo de la solicitud). 
+También tiene varios métodos que se pueden usar para obtener o cambiar ciertos valores de la solicitud.
+
+La clase HttpResponse representa una respuesta HTTP, y tiene atributos como code (código de estado HTTP), reason (razón del código de estado), headers (encabezados de la respuesta) y data (datos de la respuesta).
+También tiene varios métodos que se pueden usar para obtener ciertos valores de la respuesta.
+
+La clase Case representa un caso de prueba, y tiene atributos como vul (vulnerabilidad), method (método HTTP), url (URL de la solicitud), headers (encabezados de la solicitud), body (cuerpo de la solicitud) y args (argumentos adicionales). 
+Esta clase se utiliza para definir casos de prueba que se pueden ejecutar en una aplicación web para comprobar si hay vulnerabilidades.
+
+La primera línea (#!/usr/bin/python2.7) es un shebang que indica el intérprete de Python que se debe usar para ejecutar el script. La segunda línea (# -*- encoding: utf-8 -*-) especifica la codificación de caracteres del archivo. El resto del archivo define las clases mencionadas anteriormente. Si este archivo se ejecuta directamente (en lugar de ser importado por otro archivo), la última línea (if __name__ == '__main__':)
+ se ejecutará, pero en este caso no hace nada (pass).
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+MEJORA Y ACTUALIZACION:
+    
+ #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+
+class HttpRequest:
+    def __init__(self, method, url, headers, body=''):
+        self.method = method
+        self.url = url
+        self.headers = headers
+        self.body = body
+
+    def __str__(self):
+        return f"{self.method} {self.url}\n{self.headers2str()}\n{self.body}"
+
+    def headers2str(self):
+        return '\n'.join([f"{key}: {value}" for key, value in self.headers.items()])
+
+class HttpResponse:
+    def __init__(self, code, reason, headers, data):
+        self.code = code
+        self.reason = reason
+        self.headers = headers
+        self.data = data
+
+    def __str__(self):
+        return f"{self.code} {self.reason}\n{self.headers2str()}\n{self.data}"
+
+    def headers2str(self):
+        return '\n'.join([f"{key}: {value}" for key, value in self.headers.items()])
+
+    def get_header(self, header_name):
+        return self.headers.get(header_name.lower(), None)
+
+    def get_setcookie_list(self):
+        return self.headers.get('Set-Cookie', [])
+
+class Case:
+    def __init__(self, vul, method, url, headers, body='', args=None):
+        """
+        Case class is a object like test case, include http-util, verify function, url etc.
+        :param vul:
+        :param method:
+        :param url:
+        :param headers:
+        :param body:
+        :param args: (location, match)
+        """
+        self.vul = vul
+        self.method = method
+        self.url = url
+        self.headers = headers
+        self.body = body
+        self.args = args or {}
+
+if __name__ == '__main__':
+    pass
+    
+    
+    Aquí están las mejoras realizadas:
+
+He actualizado la sintaxis del código para Python 3.
+    
+He eliminado la función tostring() de las clases HttpRequest y HttpResponse, ya que no es necesaria.
+    
+He renombrado el método headers2print() de la clase HttpRequest a headers2str(), ya que devuelve una cadena de caracteres y no la imprime directamente.
+    
+He actualizado el método __str__() de las clases HttpRequest y HttpResponse para que utilicen el método headers2str().
+    
+He actualizado el método get_header() de la clase HttpResponse para que use el método get() en lugar de buscar la clave directamente en el diccionario de encabezados.
+    
+He actualizado el método get_setcookie_list() de la clase HttpResponse para que devuelva una lista vacía si el encabezado Set-Cookie no está presente.
+    
+He agregado un valor predeterminado para el parámetro args en la clase Case.
+    
+    
+    
