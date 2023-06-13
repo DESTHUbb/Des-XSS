1cd250e038ad406b13ea55921680b9d215592e6e
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-13 15:05:29 +0200

    Update cookie.py

--- cookie.py
+++ cookie.py
@@ -109,3 +109,158 @@ def try_cookie(domain):
 
 if __name__=='__main__':
     pass
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+DESCRIPCION:
+    Este es un script de Python que se utiliza para trabajar con cookies en aplicaciones web. 
+    El script ofrece una serie de funciones que permiten obtener cookies de un archivo de cookies, guardar cookies en un archivo, y verificar si una cookie ha caducado.
+
+El script comienza importando varios módulos de Python, incluyendo os, re y time. 
+Luego importa las variables COOKIE_DIR y LOGGER desde otros módulos llamados config y log, respectivamente.
+
+A continuación, se definen varias funciones en el script:
+
+is_ip: Esta función verifica si un dominio es una dirección IP.
+get_cookies_list: Esta función obtiene una lista de cookies para un dominio de destino. 
+La función busca un archivo de cookies en el directorio de cookies definido en COOKIE_DIR y devuelve una lista de diccionarios que representan cada cookie.
+save_cookie: Esta función guarda una cookie en un archivo de cookies en el directorio de cookies definido en COOKIE_DIR.
+save_cookie_ip: Esta función guarda una cookie en un archivo de cookies utilizando la dirección IP del servidor como nombre de archivo.
+get_cookie: Esta función obtiene una cookie para un dominio de destino. La función busca un archivo de cookies en el directorio de cookies definido en COOKIE_DIR y devuelve el valor de la cookie como una cadena.
+get_cookie_ip: Esta función obtiene una cookie utilizando la dirección IP del servidor como nombre de archivo.
+try_cookie: Esta función intenta obtener una cookie para un dominio de destino y le pregunta al usuario si desea utilizar la cookie encontrada en el archivo de cookies.
+Finalmente, el script verifica si se está ejecutando como un script independiente (name=='main') y no hace nada en ese caso.
+
+En resumen, este script de Python proporciona varias funciones para trabajar con cookies en aplicaciones web, incluyendo la obtención y el almacenamiento de cookies en archivos, y la verificación de si una cookie ha caducado.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+MEJORAS Y ACTUALIZACIONES:
+    #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Work with cookies"""
+import os
+import re
+import time
+from pathlib import Path
+from config import COOKIE_DIR
+from log import LOGGER
+
+__author__ = 'longwenzhang'
+
+def is_ip(domain):
+    """Check if a domain is an IP address"""
+    if re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}', domain):
+        return True
+
+def get_cookies_list(target_domain):
+    """
+    Get a list of cookies for a target domain
+    Returns a list of dictionaries representing each cookie
+    """
+    if '.' in target_domain:
+        cookies_list = []
+        # if the domain is an IP address
+        if is_ip(target_domain):
+            domain_scope = target_domain
+        else:
+            # default
+            domain_scope = '.' + target_domain.split('.')[-2] + '.' + target_domain.split('.')[-1]
+        cookie_file_path = Path(COOKIE_DIR) / f"{domain_scope}_cookie.txt"
+        if cookie_file_path.is_file():
+            with open(cookie_file_path, "r") as cookie_file:
+                cookie_file_list = cookie_file.readlines()
+                expire = cookie_file_list[2]
+                # check if cookie has expired
+                if int(time.time()) < int(expire):
+                    cookies_text = cookie_file_list[0].strip()
+                    domain = cookie_file_list[1].strip()
+                    new_list = cookies_text.split(';')
+                    for i in new_list:
+                        if i != '':
+                            cookie_dict = {}
+                            key = i.split('=')[0].strip()
+                            value = i.split('=')[1].strip()
+                            cookie_dict['domain'] = domain
+                            cookie_dict['name'] = key
+                            cookie_dict['value'] = value
+                            cookie_dict['path'] = '/'
+                            cookies_list.append(cookie_dict)
+        return cookies_list
+
+def save_cookie(cookie, domain, expire_time=3600):
+    """Save a cookie in a file"""
+    domain_scope = f".{domain.split('.')[-2]}.{domain.split('.')[-1]}"
+    expire = int(time.time()) + expire_time
+    cookie_file_path = Path(COOKIE_DIR) / f"{domain_scope}_cookie.txt"
+    with open(cookie_file_path, 'w+') as cookie_file:
+        cookie_file.write(f"{cookie}\n")
+        cookie_file.write(f"{domain_scope}\n")
+        cookie_file.write(str(expire))
+
+def save_cookie_ip(cookie, ip, expire_time=3600):
+    """Save a cookie for an IP address"""
+    domain_scope = ip
+    expire = int(time.time()) + expire_time
+    cookie_file_path = Path(COOKIE_DIR) / f"{domain_scope}_cookie.txt"
+    with open(cookie_file_path, 'w+') as cookie_file:
+        cookie_file.write(f"{cookie}\n")
+        cookie_file.write(f"{domain_scope}\n")
+        cookie_file.write(str(expire))
+
+def get_cookie(target_domain):
+    """Get a cookie for a target domain"""
+    if '.' in target_domain:
+        domain_scope = '.' + target_domain.split('.')[-2] + '.' + target_domain.split('.')[-1]
+        cookie_file_path = Path(COOKIE_DIR) / f"{domain_scope}_cookie.txt"
+        if cookie_file_path.is_file():
+            with open(cookie_file_path, "r") as cookie_file:
+                cookie_file_list = cookie_file.readlines()
+                expire = cookie_file_list[2]
+                # check if cookie has expired
+                if int(time.time()) < int(expire):
+                    cookies_text = cookie_file_list[0].strip()
+                    return cookies_text
+                else:
+                    LOGGER.warning(f"Cookie of {domain_scope} has expired!")
+        else:
+            LOGGER.warning(f"Cookie file for {domain_scope} does not exist!")
+            return None
+
+def get_cookie_ip(ip):
+    """Get a cookie for an IP address"""
+    domain_scope = ip
+    cookie_file_path = Path(COOKIE_DIR) / f"{domain_scope}_cookie.txt"
+    if cookie_file_path.is_file():
+        with open(cookie_file_path, "r") as cookie_file:
+            cookie_file_list = cookie_file.readlines()
+            expire = cookie_file_list[2]
+            # check if cookie has expired
+            if int(time.time()) < int(expire):
+                cookies_text = cookie_file_list[0].strip()
+                return cookies_text
+            else:
+                LOGGER.warning(f"Cookie of {domain_scope} has expired!")
+    else:
+        LOGGER.warning(f"Cookie file for {domain_scope} does not exist!")
+        return None
+
+def try_cookie(domain):
+    """Try to find a cookie for a domain and ask user if they want to use it"""
+    cookie = get_cookie(domain)
+    if cookie:
+        choose = input('\033[1Esta versión mejorada y actualizada del script de Python para trabajar con cookies en aplicaciones web incluye algunas mejoras en la estructura y la funcionalidad del script. 
+
+                       
+En primer lugar, se han agregado comentarios adicionales para ayudar a explicar el propósito y la funcionalidad de cada función.
+También se han agregado algunos detalles adicionales, como la opción de establecer un tiempo de caducidad de la cookie al guardarla en un archivo.
+
+Además, se ha cambiado la forma en que se manejan las cookies de direcciones IP y se ha agregado una nueva función para intentar obtener una cookie para un dominio y preguntar al usuario si desea utilizar la cookie encontrada en el archivo de cookies.
+
+También se ha utilizado la librería pathlib para trabajar con rutas de archivo, lo que hace que el código sea más legible y más fácil de mantener.
+
+En general, esta versión mejorada y actualizada del script ofrece una funcionalidad más completa y una estructura más clara y fácil de entender.
+
+
+
+
