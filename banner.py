c61c292d25182e5f5c1f4271704eadcd7eb82862
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-16 00:33:35 +0200

    Update banner.py

--- banner.py
+++ banner.py
@@ -1,30 +1,4 @@
-#!/usr/bin/python2.7
-# -*- encoding: utf-8 -*-
-"""
-    @Description: print logo.
-    
-    ~~~~~~ 
-    @Author  : longwenzhang
-    @Time    : 19-10-9  10:28
-"""
-import os
-from config import BASE_DIR
-
-def banner():
-    with open(os.path.join(BASE_DIR,'logo'))as banner_f:
-        a=banner_f.read()
-    BANNER = """\033[01;33m"""+a+"""\033[0m"""
-    print BANNER
-
-if __name__=='__main__':
-    banner()
-    
-    
-    /////////////////////////////////////
-    
-    new 
-    
-    #!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
     @Description: Script to print a welcome message.
@@ -34,9 +8,14 @@ if __name__=='__main__':
     @Time    : 2023-06-13  10:00
 """
 
-def welcome():
-    message = "Welcome to my Python script!"
-    print(message)
+import os
+from config import BASE_DIR
 
-if __name__ == '__main__':
-    welcome()
+def banner():
+    with open(os.path.join(BASE_DIR,'logo'))as banner_f:
+        a=banner_f.read()
+    BANNER = """\033[01;33m"""+a+"""\033[0m"""
+    print(BANNER)
+
+if __name__=='__main__':
+    banner()
