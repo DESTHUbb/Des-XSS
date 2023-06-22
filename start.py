764b66274f0a24b45374209e431b33b4dcbfd79b
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-22 18:48:36 +0200

    Update start.py

--- start.py
+++ start.py
@@ -165,3 +165,5 @@ from cookie import save_cookie, save_cookie_ip, is_ip
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="start.py", description='scan xss from url or file.', usage='start.py --url=url --save')
+        parser.add_argument('-v', '--version', action='version', version='V1.0-beta')
+
