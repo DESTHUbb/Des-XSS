df64b9c8deceb7bc313775331701893ad61c9f92
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-22 18:49:34 +0200

    Update start.py

--- start.py
+++ start.py
@@ -167,4 +167,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="start.py", description='scan xss from url or file.', usage='start.py --url=url --save')
     parser.add_argument('-v', '--version', action='version', version='V2.0-beta')
     parser.add_argument('--check', action='store_true', help='check if browser is installed correctly.')
+    parser.add_argument('--url', '-u', help='the target site of scan.')
 
