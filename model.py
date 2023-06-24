f0cc9fa9d3f5acad492f9ed611bfdaf943127558
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-24 14:15:26 +0200

    Update model.py

--- model.py
+++ model.py
@@ -154,4 +154,5 @@ class HttpResponse:
         headers_str = '\n'.join([f'{k}: {v}' for k, v in self.headers.items()])
         return f'{self.code} {self.reason}\n{headers_str}\n{self.data}'
 
-        
+ class Case:
+       
