f0033028c6181816527faf7051c23658d36596fd
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-06-26 11:38:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,3 @@
-#!/usr/bin/python2.7
-# -*- encoding: utf-8 -*-
-"""
-    @Description: Configuration of Des-XSS.
-    
-    ~~~~~~ 
-    @Author  : DESTHUbb
-    @Time    : 19-10-9  10:16
-"""
-import os
-
-# global dir
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-COOKIE_DIR = os.path.join(BASE_DIR, 'cookie')
-RESULT_DIR = os.path.join(BASE_DIR, 'result')
-TRAFFIC_DIR =os.path.join(BASE_DIR, 'traffic')
-
-# save request error
-# [(func_name,request,exception),]
-REQUEST_ERROR=[]
-
-# save redirect request
-REDIRECT=[]
-
-# save 'multipart/form-data; boundary=' request
-MULTIPART=[]
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 #python3 improvement
 
 #!/usr/bin/env python3
@@ -53,6 +24,7 @@ REDIRECT = []
 
 # Save 'multipart/form-data; boundary=' requests
 MULTIPART = []
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 # Improvements made:
