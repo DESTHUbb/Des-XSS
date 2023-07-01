2091081c9b805e06dcd0f10286aea6de5b2d6d08
Alex <90658763+DESTHUbb@users.noreply.github.com>
2023-07-01 03:32:27 +0200

    Update model.py

--- model.py
+++ model.py
@@ -117,65 +117,7 @@ if __name__ == '__main__':
 @Time    : 19-8-19   3:33
 """
 
-import json
 
-class HttpRequest:
-    def __init__(self, method, url, headers, body=''):
-        self.method = method
-        self.url = url
-        self.headers = headers
-        self.body = body
-
-    def __str__(self):
-        headers_str = '\n'.join([f'{k}: {v}' for k, v in self.headers.items()])
-        return f'{self.method} {self.url}\n{headers_str}\n{self.body}'
-        
-class HttpResponse:
-    def __init__(self, code, reason, headers, data):
-        self.code = code
-        self.reason = reason
-        self.headers = headers
-        self.data = data
-
-    def get_header(self, header_name):
-     try:
-            return self.headers[header_name]
-                 except KeyError:
-            return self.headers.get(header_name.lower())
-
-    def get_setcookie_list(self):
-        setcookie_list = []
-        for resp_header_name, resp_header_value in self.headers.items():
-            if resp_header_name.lower() == 'set-cookie':
-                setcookie_list.append(resp_header_value)
-        return setcookie_list
-        
-    def __str__(self):
-        headers_str = '\n'.join([f'{k}: {v}' for k, v in self.headers.items()])
-        return f'{self.code} {self.reason}\n{headers_str}\n{self.data}'
-
- class Case:
-           def __init__(self, vul, method, url, headers, body, args):
-               
-"""
-        Case class is a object like test case,include http-util,verify function,url etc.
-        
-        :param vul:
-        :param method:
-        :param url:
-        :param headers:
-        :param body:
-        :param args: (location,match)
-"""
-        self.vul = vul
-        self.method = method
-        self.url = url
-        self.headers = headers
-        self.body = body
-        self.args = args
-
-if __name__ == '__main__':
-    pass
 
  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
 
